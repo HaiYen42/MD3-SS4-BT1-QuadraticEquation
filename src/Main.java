05d0bb45dc03dccd73f14f859de595466448f652
HaiYen42 <haiyentth42@gmail.com>
2023-03-06 14:32:57 +0700

    Update

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double a, b, c;
-        System.out.println("Nhập a");
-        a= input.nextDouble();
-//        if (a==0){
-//            System.out.println("Vui lòng nhập a khác 0");
-//            System.out.println("Nhập a");
-//            a= input.nextDouble();
-//        }
-        while(a==0) {
-            System.out.println("Vui lòng nhập a khác 0");
-            System.out.println("Nhập a");
-        }
-        do {
-            System.out.print("Nhập a: ");
+
+        do{
+            System.out.println("Enter a");
             a= input.nextDouble();
             if (a==0){
-                System.out.println("Vui lòng nhập a khác 0");
+                System.out.println("VUi lòng nhập a khác 0");
             }
-        } while(a==0);
-        System.out.print("Nhập b: ");
+
+        }while (a==0);
+        System.out.println("Enter b");
         b= input.nextDouble();
-        System.out.println("Nhập c: ");
+        System.out.println("Enter c");
         c= input.nextDouble();
-        QuadraticEquation equation = new QuadraticEquation(a, b, c);
-            double delta= equation.getDiscriminant();
-            double root1= equation.getRoot1();
-            double root2= equation.getRoot2();
-        if (delta<0){
-            System.out.println(" Phương trình vô nghiệm");
-        }else if(a==0){
-            System.out.println("Phương trình có 1 nghệm là "+ root1);
+        QuadraticEquation equation= new QuadraticEquation(a,b,c);
+        double delta= equation.getDiscriminant();
+        double root1= equation.getRoot1();
+        double root2= equation.getRoot2();
+        if (delta==0){
+            System.out.println("Phương trình vô nghiệm");
+        }else if (delta==0){
+            System.out.println("Phương trình có 1 nghiệm là "+ root1);
         }else{
             System.out.println("Phương trình có 2 nghiệm là "+ root1+ " "+ root2);
         }
--- src/QuadraticEquation.java
+++ src/QuadraticEquation.java
@@ -1,14 +1,59 @@
 public class QuadraticEquation {
-    private double a, b, c;
+//    private double a, b, c;
+//
+//  public QuadraticEquation(){
+//
+//  }
+//  public QuadraticEquation (double a, double b, double c){
+//      this.a= a;
+//      this.b= b;
+//      this.c = c;
+//  }
+//
+//    public double getA() {
+//        return a;
+//    }
+//
+//    public double getB() {
+//        return b;
+//    }
+//
+//    public double getC() {
+//        return c;
+//    }
+//    public double getDiscriminant(){
+//      return Math.pow(b, 2 )- (4*a*c) ;
+//
+//    }
+//    public double getRoot1(){
+//      double x1;
+//      double delta= getDiscriminant();
+//      if (delta<0){
+//          return x1=0;
+//      }else{
+//          x1= (-b+Math.sqrt(delta))/(2*a);
+//          return x1;
+//      }
+//    }
+//    public double getRoot2(){
+//      double x2;
+//      double delta = getDiscriminant();
+//      if (delta<0){
+//          return x2=0;
+//      }else{
+//           x2= (-b-Math.sqrt(delta))/(2*a);
+//           return x2;
+//      }
+//    }
+private double a, b, c;
+public QuadraticEquation(){
 
-  public QuadraticEquation(){
-
-  }
-  public QuadraticEquation (double a, double b, double c){
-      this.a= a;
-      this.b= b;
-      this.c = c;
-  }
+}
+public QuadraticEquation(double a, double b, double c){
+    this.a= a;
+    this.b= b;
+    this.c = c;
+}
 
     public double getA() {
         return a;
@@ -22,27 +67,27 @@ public class QuadraticEquation {
         return c;
     }
     public double getDiscriminant(){
-      return Math.pow(b, 2 )- (4*a*c) ;
-
+    return Math.pow(b,2)- (4*a*c);
     }
     public double getRoot1(){
-      double x1;
-      double delta= getDiscriminant();
-      if (delta<0){
-          return x1=0;
-      }else{
-          x1= (-b+Math.sqrt(delta))/(2*a);
-          return x1;
-      }
+    double x1;
+    double delta= getDiscriminant();
+    if (delta<0){
+        return x1=0;
+    }else {
+        x1 = (-b + Math.sqrt(delta) )/ (2 * a);
+        return x1;
+    }
     }
     public double getRoot2(){
-      double x2;
-      double delta = getDiscriminant();
-      if (delta<0){
-          return x2=0;
-      }else{
-           x2= (-b-Math.sqrt(delta))/(2*a);
-           return x2;
-      }
+    double x2;
+    double delta= getDiscriminant();
+    if (delta<0){
+        return x2=0;
+    }else{
+        x2= (-b-Math.sqrt(delta))/(2*a);
+        return x2;
+    }
     }
 }
+
